1e9d1527d8816e8fe6d57434f5e4ffff0f8d5e3a
criccadamus <contact@criccadamus.eu>
2026-01-29 21:40:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SharedMedia_Pretendard
+
+- Add the Pretendard fonts to `libsharedmedia`-compatible addons.
+
+![Details example](images/details-example.png)
+
+## Pretendard?
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://github.com/orioncactus/pretendard/blob/HEAD/docs/images/thumbnail/dark/thumbnail.svg">
+  <img src="https://github.com/orioncactus/pretendard/blob/HEAD/docs/images/thumbnail/light/thumbnail.svg" alt="Thumbnail">
+</picture>
+
+- Pretendard is a modern, cross-platform system-ui alternative font based on Inter, Source Han Sans, and M PLUS 1p.
+- Supports Korean (KR) and Western languages.
+
+## Download Links
+
+- [Pretendard repo](https://github.com/orioncactus/pretendard)
---
+++ SharedMedia_Pretendard.lua
@@ -0,0 +1,15 @@
+
+local LSM = LibStub("LibSharedMedia-3.0")
+local koKR, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_western
+local flag = koKR + western
+local FONT = LSM.MediaType.FONT
+
+LSM:Register(FONT, "Pretendard Thin",			[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-Thin.otf]], flag)
+LSM:Register(FONT, "Pretendard ExtraLight",		[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-ExtraLight.otf]], flag)
+LSM:Register(FONT, "Pretendard Light",			[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-Light.otf]], flag)
+LSM:Register(FONT, "Pretendard Regular",		[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-Regular.otf]], flag)
+LSM:Register(FONT, "Pretendard Medium",			[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-Medium.otf]], flag)
+LSM:Register(FONT, "Pretendard SemiBold",		[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-SemiBold.otf]], flag)
+LSM:Register(FONT, "Pretendard Bold",			[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-Bold.otf]], flag)
+LSM:Register(FONT, "Pretendard ExtraBold",		[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-ExtraBold.otf]], flag)
+LSM:Register(FONT, "Pretendard Black",			[[Interface\Addons\SharedMedia_Pretendard\Fonts\Pretendard-Black.otf]], flag)
